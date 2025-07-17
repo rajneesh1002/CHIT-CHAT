6088baff0a088ce2659b973289c4ef4e1bf5e5a3
rajneesh <yadavd.ug20.cse@nitp.ac.in>
2025-07-17 16:55:22 +0530

    fixing error

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,15 +1,14 @@
 import jwt from 'jsonwebtoken';
 
-export const generateToken=(userId,res)=>{
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '7d',
+  });
 
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:'7d'})
-
-    res.cookie("jwt",token, {
-        maxAge: 7*24*60*60*1000, //maxAge max-age in milliseconds, converted to expires
-        httpOnly: true, //prevent xss attacks cross-site scripting attacts
-        samsSite: 'strict', // CSRF attacts cross-site request forgery attacts
-        sucure: process.env.NODE_ENV !=='development',
-    });
-
-
-}
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,                 
+    sameSite: "None",               
+    secure: process.env.NODE_ENV !== "development", 
+  });
+};
