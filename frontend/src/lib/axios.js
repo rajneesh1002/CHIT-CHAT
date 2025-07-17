291f2aecde43a006c3198e5a5a32ec33afca42bd
rajneesh <yadavd.ug20.cse@nitp.ac.in>
2025-07-17 16:16:06 +0530

    changing URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const axiosInstance=axios.create({
-    baseURL: import.meta.env.MODE==="development" ? "http://localhost:5001/api" : "/api" ,
-    withCredentials: true,
-})
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:5001/api" // local dev
+    : import.meta.env.VITE_BACKEND_URL + "/api", // deployed backend
+  withCredentials: true,
+});
