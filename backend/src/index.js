57fd32d204cffc7e48560e77426e95e0e87d5f48
rajneesh <yadavd.ug20.cse@nitp.ac.in>
2025-07-19 12:44:43 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
 
 app.use(cookieParser());
 const allowedOrigins = [
